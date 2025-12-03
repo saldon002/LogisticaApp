9cefc5758f18e11341d7190b281fe90a09f2cf03
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-03 19:55:41 +0100

    Create PackingStrategy.java

---
+++ src/main/java/it/prog3/logisticaapp/business/PackingStrategy.java
@@ -0,0 +1,23 @@
+package it.prog3.logisticaapp.business;
+
+import it.prog3.logisticaapp.model.ICollo;
+import it.prog3.logisticaapp.model.IVeicolo;
+import java.util.List;
+
+/**
+ * Interfaccia del pattern Strategy per gli algoritmi di Bin Packing.
+ * <p>
+ * Definisce il metodo comune che tutti gli algoritmi (NextFit, BestFit, etc.)
+ * Dovranno implementare per distribuire i colli nei veicoli.
+ * </p>
+ */
+public interface PackingStrategy {
+
+    /**
+     * Esegue l'algoritmo di riempimento.
+     *
+     * @param colli  La lista dei colli da caricare.
+     * @param flotta La lista dei veicoli disponibili.
+     */
+    void eseguiPacking(List<ICollo> colli, List<IVeicolo> flotta);
+}
