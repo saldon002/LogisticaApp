97a549a3f8f7eceffa431daee30d690b40cf58c1
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-03 17:05:43 +0100

    Create HelloController.java

---
+++ src/main/java/it/prog3/logisticaapp/HelloController.java
@@ -0,0 +1,14 @@
+package it.prog3.logisticaapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
