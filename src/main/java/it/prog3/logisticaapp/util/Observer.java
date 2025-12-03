bf10cb4f478004c3ea17408b04ad79b60f0518fc
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-03 17:24:40 +0100

    Update Observer.java

--- src/main/java/it/prog3/logisticaapp/util/Observer.java
+++ src/main/java/it/prog3/logisticaapp/util/Observer.java
@@ -1,4 +1,21 @@
 package it.prog3.logisticaapp.util;
 
-public class Observer {
+import javax.security.auth.Subject;
+
+/**
+ * Rappresenta l'astrazione dell'Osservatore nel pattern Observer.
+ * <p>
+ * Le classi che estendono Observer devono implementare
+ * il metodo {@code update()} per reagire ai cambiamenti di stato del Subject.
+ * </p>
+ */
+public abstract class Observer {
+    private Subject subject;
+    public Subject getSubject() {
+        return subject;
+    }
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+    public abstract void update();
 }
