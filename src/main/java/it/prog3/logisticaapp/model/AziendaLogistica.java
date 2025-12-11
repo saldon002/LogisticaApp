150952db9997b5cb79e858ac12c59bbda715b834
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-11 23:24:31 +0100

    Update AziendaLogistica.java

--- src/main/java/it/prog3/logisticaapp/model/AziendaLogistica.java
+++ src/main/java/it/prog3/logisticaapp/model/AziendaLogistica.java
@@ -7,15 +7,15 @@ package it.prog3.logisticaapp.model;
 public class AziendaLogistica extends AziendaTrasporti {
 
     @Override
-    public IVeicolo createVeicolo(String tipo, String codice, String azienda) {
+    public IVeicolo createVeicolo(String tipo, String codice) {
         // Logica parametrica come da slide
         if (tipo == null) return null;
 
         if (tipo.equalsIgnoreCase("CAMION")) {
-            return new Camion(codice, azienda);
+            return new Camion(codice);
         }
         else if (tipo.equalsIgnoreCase("FURGONE")) {
-            return new Furgone(codice, azienda);
+            return new Furgone(codice);
         }
 
         return null; // Tipo sconosciuto
