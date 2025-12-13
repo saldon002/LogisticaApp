fad5126a46a47b5ac0548806a2481b0aa6c26b1a
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-13 19:30:18 +0100

    Update Furgone.java

--- src/main/java/it/prog3/logisticaapp/model/Furgone.java
+++ src/main/java/it/prog3/logisticaapp/model/Furgone.java
@@ -2,13 +2,17 @@ package it.prog3.logisticaapp.model;
 
 public class Furgone extends VeicoloAstratto {
 
+    // Costante per la capienza standard (opzionale ma pulita)
+    private static final int CAPIENZA_STANDARD = 20;
+
     public Furgone() {
         super();
-        this.setCapienza(20);
+        // Usiamo il setter per attivare eventuali validazioni del padre
+        this.setCapienza(CAPIENZA_STANDARD);
     }
 
     public Furgone(String codice) {
-        super(codice, 20);
+        super(codice, CAPIENZA_STANDARD);
     }
 
     @Override
