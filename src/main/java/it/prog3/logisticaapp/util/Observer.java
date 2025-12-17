cf511955c82a7c43dea6fbfb7e3cdad527b418fe
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-17 19:02:39 +0100

    Update Observer.java

--- src/main/java/it/prog3/logisticaapp/util/Observer.java
+++ src/main/java/it/prog3/logisticaapp/util/Observer.java
@@ -1,19 +1,15 @@
 package it.prog3.logisticaapp.util;
 
 /**
- * Interfaccia per il pattern Observer.
+ * Interfaccia Observer (parte del pattern Observer).
  * <p>
  * Definisce il contratto per gli oggetti che vogliono essere notificati
- * dei cambiamenti di stato di un Subject.
- * Trasformata in interfaccia per permettere ai Controller JavaFX di implementarla
- * senza vincoli di ereditarietà singola.
+ * dei cambiamenti di stato in un oggetto Subject.
  * </p>
  */
 public interface Observer {
-
     /**
      * Metodo chiamato dal Subject quando il suo stato cambia.
-     * Le classi concrete (es. MainController) definiranno qui la logica di reazione.
      */
     void update();
 }
