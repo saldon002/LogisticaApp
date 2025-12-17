fa81564ed6862ddb3614cc96af69fcb7d31dc627
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-17 19:02:37 +0100

    Update IObservable.java

--- src/main/java/it/prog3/logisticaapp/util/IObservable.java
+++ src/main/java/it/prog3/logisticaapp/util/IObservable.java
@@ -2,7 +2,9 @@ package it.prog3.logisticaapp.util;
 
 /**
  * Interfaccia segregata per il pattern Observer (ISP).
- * Definisce solo i metodi necessari alla gestione degli osservatori.
+ * <p>
+ *  Definisce solo i metodi necessari alla gestione degli osservatori.
+ * </p>
  */
 public interface IObservable {
     void attach(Observer o);
