d7edd090780f04a4d97b5b5872e3ac341cb8f75b
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-19 19:55:19 +0100

    Update DbSetup.java

--- src/main/java/it/prog3/logisticaapp/database/DbSetup.java
+++ src/main/java/it/prog3/logisticaapp/database/DbSetup.java
@@ -60,34 +60,6 @@ public class DbSetup {
             stmt.executeUpdate(sqlStorico);
             System.out.println("-> Tabella 'storico_spostamenti' creata.");
 
-            // ==========================================
-            // 3. INSERIMENTO DATI DI PROVA (MOCK DATA)
-            // ==========================================
-            // Nota: Qui usiamo Statement semplice perché i dati sono costanti e sicuri.
-            // In un'app reale con input utente useremmo sempre PreparedStatement.
-
-            // A. INSERIMENTO FLOTTA
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V01', 'CAMION',  5, 'DHL')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V02', 'FURGONE', 2, 'DHL')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V03', 'CAMION',  5, 'DHL')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V04', 'FURGONE', 2, 'DHL')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V05', 'CAMION',  5, 'DHL')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V06', 'CAMION',  5, 'BARTOLINI')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V07', 'FURGONE', 2, 'BARTOLINI')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V08', 'CAMION',  5, 'BARTOLINI')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V09', 'FURGONE', 2, 'BARTOLINI')");
-            stmt.executeUpdate("INSERT INTO veicoli VALUES ('V10', 'CAMION',  5, 'BARTOLINI')");
-            System.out.println("-> Dati veicoli inseriti.");
-
-            // B. INSERIMENTO COLLI
-            for (int i = 1; i <= 25; i++) {
-                String codice = String.format("C%02d", i); // Genera C_01, C_02...
-
-                String sql = String.format("INSERT INTO colli VALUES ('%s', 1.0, 'IN_PREPARAZIONE', 'X', 'X')", codice);
-                stmt.executeUpdate(sql);
-            }
-            System.out.println("-> Dati colli inseriti.");
-
             System.out.println("=== SETUP COMPLETATO CON SUCCESSO ===");
 
         } catch (SQLException e) {
