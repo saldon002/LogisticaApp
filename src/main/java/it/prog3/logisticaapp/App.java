f825bcb0dad59f75695c15736f03b69cbcfa03b7
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2025-12-20 12:52:36 +0100

    Update App.java

--- src/main/java/it/prog3/logisticaapp/App.java
+++ src/main/java/it/prog3/logisticaapp/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Carica la vista di Login
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/manager.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/corriere.fxml"));
         Parent root = fxmlLoader.load();
 
         scene = new Scene(root, 600, 400);
