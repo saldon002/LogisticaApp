4f9e639193edf0825bb1b7c2005d3add50b3a5d2
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2026-01-16 19:31:31 +0100

    Update IDataLoader.java

--- src/main/java/it/prog3/logisticaapp/database/IDataLoader.java
+++ src/main/java/it/prog3/logisticaapp/database/IDataLoader.java
@@ -2,6 +2,19 @@ package it.prog3.logisticaapp.database;
 
 import it.prog3.logisticaapp.model.ColloReale;
 
+/**
+ * Interfaccia per il caricamento dei dati (Dependency Inversion Principle).
+ * <p>
+ * Questa astrazione permette al Proxy di non dipendere direttamente dalla classe concreta GestoreDatabase.
+ * </p>
+ */
 public interface IDataLoader {
+
+    /**
+     * Recupera l'oggetto reale completo (dati + storico).
+     *
+     * @param codice Il codice univoco del collo.
+     * @return L'istanza di ColloReale popolata, o null se non trovato.
+     */
     ColloReale getColloRealeCompleto(String codice);
 }
