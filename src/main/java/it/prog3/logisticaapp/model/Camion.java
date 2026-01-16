d13fd7134ed6a6b48e6a666c1297358e39fe8720
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2026-01-16 19:31:37 +0100

    Update Camion.java

--- src/main/java/it/prog3/logisticaapp/model/Camion.java
+++ src/main/java/it/prog3/logisticaapp/model/Camion.java
@@ -2,12 +2,7 @@ package it.prog3.logisticaapp.model;
 
 public class Camion extends VeicoloAstratto {
 
-    private static final int CAPIENZA_STANDARD = 5; // Capienza maggiore
-
-    public Camion() {
-        super();
-        this.setCapienza(CAPIENZA_STANDARD);
-    }
+    private static final int CAPIENZA_STANDARD = 5;
 
     public Camion(String codice) {
         super(codice, CAPIENZA_STANDARD);
